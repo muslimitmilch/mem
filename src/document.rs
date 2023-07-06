3d43e4f45718766bf40579d826ef02249aa136c1
muslimitmilch <malte.stollberg@outlook.de>
2023-07-06 09:31:28 +0200

    showing text

---
+++ src/document.rs
@@ -0,0 +1,31 @@
+pub struct Document {
+    rows: Vec<Row>,
+}
+
+impl Document {
+    pub fn open() -> Self {
+        let mut rows = Vec::new();
+        rows.push(Row::from("hello world"));
+        rows.push(Row::from("zweite zeile"));
+        Self {rows}
+    }
+
+    pub fn rows(&self) -> &Vec<Row> {
+       &self.rows 
+    }
+}
+
+
+
+pub struct Row {
+    string: String,
+}
+
+impl Row {
+    fn from(slice: &str) -> Self {
+        Self {string: String::from(slice)}
+    }
+    pub fn string(&self) -> &String {
+        &self.string
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod editor;
 mod terminal;
+mod document;
 
 use editor::Editor;
 pub use terminal::Terminal;
+pub use document::Document;
 
 fn main() {
     let mut editor = Editor::default();
